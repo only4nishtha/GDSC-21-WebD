6305c8e8ddba8d2760b32e3bed58bfcc24970a08
Nishtha Agarwal <77295838+only4nishtha@users.noreply.github.com>
2021-09-30 18:57:46 +0530

    Gradient-Type

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 var css = document.querySelector("h4");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var ratio = document.querySelector(".ratio");
+var type = document.querySelector("#type");
 var gradient = document.getElementById("gradient");
+console.log(ratio.value);
 
 function setGradient() {
- gradient.style.background = 
- "linear-gradient(45deg, " 
- + color1.value 
- + ", " 
- + color2.value 
- + ")";
- css.textContent = gradient.style.background + ";";
+    if(type.value == "linear-gradient"){
+        gradient.style.background = 
+        "linear-gradient("
+       + ratio.value + "deg"
+        + ", " 
+        + color1.value 
+        + ", " 
+        + color2.value 
+        + ")";
+        css.innerText = gradient.style.background + ";";
+    }
+    else{
+        gradient.style.background = 
+        "radial-gradient("
+        + color1.value 
+        + ", " 
+        + color2.value 
+        + ")";
+        css.innerText = gradient.style.background + ";";
+       }
 }
 
+console.log(type.value, "3");
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+ratio.addEventListener("input", setGradient);
+type.addEventListener("input", setGradient);
 
 select('#switch').addEventListener('click', () => {
     // select('body').toggleClass('dark')
@@ -23,25 +42,3 @@ select('#switch').addEventListener('click', () => {
     storage('set', "mode", checkForClass('body', 'dark') ? 'light' : 'dark')
     select('body').css.toggle('dark')
 })
-
-
-// Wait for document to load
-document.addEventListener("DOMContentLoaded", function(event) {
-document.documentElement.setAttribute("data-theme", "light");
-
-// Get our button switcher
-var themeSwitcher = document.getElementById("theme-switcher");
-            
-// When our button gets clicked
-themeSwitcher.onclick = function() {
-    // Get the current selected theme, on the first run
-    // it should be `light`
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-            
-    // Switch between `dark` and `light`
-    var switchToTheme = currentTheme === "dark" ? "light" : "dark"
-            
-    // Set our currenet theme to the new one
-    document.documentElement.setAttribute("data-theme", switchToTheme);
-    }
-});
